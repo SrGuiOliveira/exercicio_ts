dcbb5f4028507269256ea041b192059a03695a53
Gui Oliveira <gui.live012@gmail.com>
2024-12-16 14:53:22 -0300

    Finalizado

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 "use strict";
+const multiplicar = (num1, num2) => num1 * num2;
+console.log(multiplicar(10, 5));
+const saudacao = (nome) => 'Olá, ' + nome + '. Tudo bem?';
+console.log(saudacao('Guilherme'));
---
+++ main.ts
@@ -0,0 +1,7 @@
+const multiplicar = (num1: number, num2: number): number => num1 * num2;
+
+console.log(multiplicar(10, 5));
+
+const saudacao = (nome: string): string => 'Olá, ' + nome + '. Tudo bem?';
+
+console.log(saudacao('Guilherme'));
